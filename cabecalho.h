eee858f0f21fd022b04193b6f726f8746fdede27
Gabriel Tetzner Menegueti <tetzdesen@gmail.com>
2025-07-25 13:04:42 -0300

    Atualização das heuristicas de busca local

--- cabecalho.h
+++ cabecalho.h
@@ -7,8 +7,6 @@
 typedef struct tSolucao{
     int mat_sol[MAX_VEI][MAX_CLI];
     int vet_qtd_cli_vei[MAX_VEI];
-    int vet_sol[MAX_CLI+MAX_VEI+1];
-
     int vet_pes_vei[MAX_VEI];
     double fo;
 }Solucao;
@@ -17,11 +15,24 @@ int num_cli, num_vei;
 int vet_dem_cli[MAX_CLI];
 int vet_cap_vei[MAX_VEI];
 double mat_custo[MAX_CLI][MAX_CLI];
+int visitado[MAX_CLI];
 
-void criar_solucao(Solucao& s);
+void heu_cons_ale(Solucao& s);
+int cliente_maior_demanda();
+int cliente_menor_custo(int linha);
+void heu_cons_gul(Solucao& s);
+void heu_cons_ale_gul(Solucao& s, const double& percentual);
 
-void calcular_fo2(Solucao& s);
+void heu_BL_rand(Solucao &s, const int& iter);
+void heu_BL_MM(Solucao &s);
+void heu_BL_PM(Solucao &s);
+
+void remover_cliente(Solucao& s, const int& vei, const int& pos);
+void inserir_cliente(Solucao& s, const int& vei, const int& pos, const int& cli);
+
+void gerar_vizinho(Solucao& s);
 void calcular_fo(Solucao& s);
+void escrever_solucao(Solucao &s);
 void testar_dados(char* arq);
 void ler_dados(char* arq);
 
--- main.cpp
+++ main.cpp
@@ -14,85 +14,355 @@ const int PESO_CAP = 100;
 
 int main()
 {
+    //srand(time(0));
     Solucao sol;
-    ler_dados("instancia.txt");
+    ler_dados("instancia-toy.txt");
     testar_dados(" ");
 
-    /*
-    sol.mat_sol[0][0] = 1;
-    sol.mat_sol[0][1] = 3;
-    sol.mat_sol[0][2] = 5;
-    sol.mat_sol[1][0] = 2;
-    sol.mat_sol[1][1] = 4;
-    sol.vet_qtd_cli_vei[0] = 3;
-    sol.vet_qtd_cli_vei[1] = 2;
+    Solucao s;
+    heu_cons_ale_gul(s, 8);
+    calcular_fo(s);
 
-    sol.vet_sol[0] = 0;
-    sol.vet_sol[1] = 1;
-    sol.vet_sol[2] = 3;
-    sol.vet_sol[3] = 5;
-    sol.vet_sol[4] = 0;
-    sol.vet_sol[5] = 2;
-    sol.vet_sol[6] = 4;
-    sol.vet_sol[7] = 0;
-*/
+    escrever_solucao(s);
 
-    criar_solucao(sol);
-    calcular_fo(sol);
-
-    printf("\n\n\n");
-    for (int i = 0; i < num_vei; i++)
-    {
-        for (int j = 0; j < sol.vet_qtd_cli_vei[i]; j++)
-            printf("%d ", sol.mat_sol[i][j]);
-        printf("\n");
-    }
-    printf("\nQTD: ");
-    for (int i = 0; i < num_vei; i++)
-        printf("%d ", sol.vet_qtd_cli_vei[i]);
-
-    printf("\nPESO: ");
-    for (int i = 0; i < num_vei; i++)
-        printf("%d ", sol.vet_pes_vei[i]);
-
-    printf("\n\nFO: %.2f\n", sol.fo);
-
-
-    calcular_fo2(sol);
-    printf("\n\nFO: %.2f\n", sol.fo);
+   // heu_BL_rand(s, 3 * (num_cli * num_vei));
+    heu_BL_MM(s);
+    escrever_solucao(s);
 
     return 0;
 }
 
-void criar_solucao(Solucao& s)
+void heu_cons_ale(Solucao& s)
 {
-    int vei = 0;
     memset(&s.vet_qtd_cli_vei, 0, sizeof(s.vet_qtd_cli_vei));
-    for (int j = 1; j <= num_cli; j++)
+    memset(&s.vet_pes_vei, 0, sizeof(s.vet_pes_vei));
+    memset(visitado, 0, sizeof(visitado));
+
+    visitado[0] = 1;
+
+    int counter = 0, cli, vei, pos;
+    while (counter < num_cli)
     {
-        vei = vei % num_vei;
-        s.mat_sol[vei][s.vet_qtd_cli_vei[vei]] = j;
+        cli = 1 + rand() % num_cli;
+        if (visitado[cli]) continue;
+
+        vei = rand() % num_vei;
+        pos = s.vet_qtd_cli_vei[vei];
+
+        s.mat_sol[vei][pos] = cli;
         s.vet_qtd_cli_vei[vei]++;
-        vei++;
-        //if (vei == num_vei)
-        //    vei = 0;
+        s.vet_pes_vei[vei] += vet_dem_cli[cli];
+        visitado[cli] = 1;
+        counter++;
     }
 }
 
-void calcular_fo2(Solucao& s)
+int cliente_maior_demanda()
 {
-    int vei = 0;
-    s.fo = 0;
-    memset(&s.vet_pes_vei, 0, sizeof(s.vet_pes_vei));
-    for (int j = 1; j < num_cli + num_vei + 1; j++)
+    int maior = -1;
+    int indice = -1;
+
+    for (int i = 1; i <= num_cli; i++)
     {
-        if (s.vet_sol[j] == 0)
-            vei++;
-        s.fo += mat_custo[s.vet_sol[j-1]][s.vet_sol[j]];
-        s.vet_pes_vei[vei] += vet_dem_cli[s.vet_sol[j]];
+        if (vet_dem_cli[i] > maior)
+        {
+            maior = vet_dem_cli[i];
+            indice = i;
+        }
     }
-    for (int i = 0; i < num_vei; i++)
-        s.fo += PESO_CAP * MAX(0, s.vet_pes_vei[i] - vet_cap_vei[i]);
+
+    return indice;
+}
+
+int cliente_menor_custo(int linha)
+{
+    int menor = 2147483647;
+    int indice = -1;
+
+    for (int j = 1; j <= num_cli; j++)
+    {
+        if (j == linha || visitado[j]) continue;
+
+        if (mat_custo[linha][j] < menor)
+        {
+            menor = mat_custo[linha][j];
+            indice = j;
+        }
+    }
+
+    return indice;
+}
+
+void heu_cons_gul(Solucao& s)
+{
+    memset(&s.vet_qtd_cli_vei, 0, sizeof(s.vet_qtd_cli_vei));
+    memset(s.vet_pes_vei, 0, sizeof(s.vet_pes_vei));
+    memset(visitado, 0, sizeof(visitado));
+
+    visitado[0] = 1;
+
+    int cli_maior = cliente_maior_demanda();
+    int cli_menor;
+
+    // Aloca o cliente de maior demanda primeiro
+    for (int j = 0; j < num_vei; j++)
+    {
+        if (visitado[cli_maior]) break;
+        if (vet_dem_cli[cli_maior] <= vet_cap_vei[j])
+        {
+            int pos = s.vet_qtd_cli_vei[j];
+            s.mat_sol[j][pos] = cli_maior;
+            s.vet_qtd_cli_vei[j]++;
+            s.vet_pes_vei[j] += vet_dem_cli[cli_maior];
+            visitado[cli_maior] = 1;
+            break;
+        }
+    }
+
+    // Aloca os demais clientes baseando-se no custo relativo
+    int clientes_alocados = 1;
+    while (clientes_alocados < num_cli)
+    {
+        cli_menor = cliente_menor_custo(cli_maior); // recalcula sempre
+
+        // se todos os possíveis foram visitados, termina
+        if (visitado[cli_menor]) break;
+
+        int alocado = 0;
+        for (int j = 0; j < num_vei; j++)
+        {
+            if (s.vet_pes_vei[j] + vet_dem_cli[cli_menor] <= vet_cap_vei[j])
+            {
+                int pos = s.vet_qtd_cli_vei[j];
+                s.mat_sol[j][pos] = cli_menor;
+                s.vet_qtd_cli_vei[j]++;
+                s.vet_pes_vei[j] += vet_dem_cli[cli_menor];
+                visitado[cli_menor] = 1;
+                clientes_alocados++;
+                cli_maior = cli_menor; // atualiza referência para próxima busca
+                alocado = 1;
+                break;
+            }
+        }
+
+        if (!alocado) break; // não foi possível alocar o cliente atual
+    }
+}
+
+
+void heu_cons_ale_gul(Solucao& s, const double& percentual)
+{
+
+    int qtd = MAX(2, (percentual / 100.00) * num_cli);
+    memset(&s.vet_qtd_cli_vei, 0, sizeof(s.vet_qtd_cli_vei));
+    memset(&s.vet_pes_vei, 0, sizeof(s.vet_pes_vei));
+    memset(visitado, 0, sizeof(visitado));
+
+    visitado[0] = 1;
+
+    int counter = 0, cli, vei, pos;
+    while (counter < qtd)
+    {
+        cli = 1 + rand() % qtd;
+        if (visitado[cli]) continue;
+
+        vei = rand() % num_vei;
+        pos = s.vet_qtd_cli_vei[vei];
+
+        s.mat_sol[vei][pos] = cli;
+        s.vet_qtd_cli_vei[vei]++;
+        s.vet_pes_vei[vei] += vet_dem_cli[cli];
+        visitado[cli] = 1;
+        counter++;
+    }
+
+    int cli_maior = cliente_maior_demanda();
+    int cli_menor;
+
+    // Aloca o cliente de maior demanda primeiro
+    for (int j = 0; j < num_vei; j++)
+    {
+        if (visitado[cli_maior]) break;
+        if (vet_dem_cli[cli_maior] <= vet_cap_vei[j])
+        {
+            int pos = s.vet_qtd_cli_vei[j];
+            s.mat_sol[j][pos] = cli_maior;
+            s.vet_qtd_cli_vei[j]++;
+            s.vet_pes_vei[j] += vet_dem_cli[cli_maior];
+            visitado[cli_maior] = 1;
+            break;
+        }
+    }
+
+    // Aloca os demais clientes baseando-se no custo relativo
+    int clientes_alocados = qtd+1;
+    while (clientes_alocados < num_cli)
+    {
+        cli_menor = cliente_menor_custo(cli_maior); // recalcula sempre
+
+        // se todos os possíveis foram visitados, termina
+        if (visitado[cli_menor]) break;
+
+        int alocado = 0;
+        for (int j = 0; j < num_vei; j++)
+        {
+            if (s.vet_pes_vei[j] + vet_dem_cli[cli_menor] <= vet_cap_vei[j])
+            {
+                int pos = s.vet_qtd_cli_vei[j];
+                s.mat_sol[j][pos] = cli_menor;
+                s.vet_qtd_cli_vei[j]++;
+                s.vet_pes_vei[j] += vet_dem_cli[cli_menor];
+                visitado[cli_menor] = 1;
+                clientes_alocados++;
+                cli_maior = cli_menor; // atualiza referência para próxima busca
+                alocado = 1;
+                break;
+            }
+        }
+
+        if (!alocado) break; // não foi possível alocar o cliente atual
+    }
+
+}
+
+void heu_BL_rand(Solucao &s, const int& iter)
+{
+    Solucao viz;
+    memcpy(&viz, &s, sizeof(Solucao));
+    while(true)
+    {
+        int flag = 1;
+
+        for(int i = 0; i < iter; i++)
+        {
+            int vei1, vei2;
+            do
+            {
+                vei1 = rand() % num_vei;
+                vei2 = rand() % num_vei;
+            }
+            while(vei1 == vei2 && s.vet_qtd_cli_vei[vei1] == 0);
+
+            int pos_cli1 = rand() % s.vet_qtd_cli_vei[vei1], pos_cli2;
+
+            int cli = s.mat_sol[vei1][pos_cli1];
+
+            remover_cliente(viz, vei1, pos_cli1);
+
+            int qtd = viz.vet_qtd_cli_vei[vei2];
+
+            if(qtd == 0) pos_cli2 = 0;
+            else pos_cli2 = rand() % qtd;
+
+            inserir_cliente(viz, vei2, pos_cli2, cli);
+
+            calcular_fo(viz);
+
+            if(viz.fo < s.fo)
+            {
+                memcpy(&s, &viz, sizeof(Solucao));
+                flag = 0;
+            }
+            else
+            {
+                memcpy(&viz, &s, sizeof(Solucao));
+            }
+        }
+        if(flag) break;
+    }
+}
+
+void heu_BL_MM(Solucao &s)
+{
+    Solucao viz, MM;
+
+    memcpy(&MM, &s, sizeof(Solucao));
+    while(true)
+    {
+        int flag = 1;
+        memcpy(&viz, &s, sizeof(Solucao));
+        for(int i = 0; i < num_vei; i++)
+        {
+            for(int j = 0; j < viz.vet_qtd_cli_vei[i]; j++)
+            {
+                int cli = viz.mat_sol[i][j];
+                remover_cliente(viz, i, j);
+                for(int k = 0; k < num_vei; k++)
+                {
+                    for(int l = 0; l <= viz.vet_qtd_cli_vei[k]; l++)
+                    {
+                        inserir_cliente(viz, k, l, cli);
+                        calcular_fo(viz);
+
+                        if(viz.fo < MM.fo)
+                        {
+                            memcpy(&MM, &viz, sizeof(Solucao));
+                            flag = 0;
+                        }
+                        remover_cliente(viz, k, l);
+                    }
+                }
+                inserir_cliente(viz, i, j, cli);
+            }
+        }
+        if(flag) break;
+        memcpy(&s, &MM, sizeof(Solucao));
+    }
+    memcpy(&s, &MM, sizeof(Solucao)); // ao final, retorna a melhor global
+}
+
+
+void heu_BL_PM(Solucao &s)
+{
+    Solucao viz;
+    INICIO : ;
+    memcpy(&viz, &s, sizeof(Solucao));
+
+    for(int i = 0; i < num_vei; i++)
+    {
+        if (viz.vet_qtd_cli_vei[i] == 0) continue;
+        for(int j = 0; j < viz.vet_qtd_cli_vei[i]; j++)
+        {
+            int cli = viz.mat_sol[i][j];
+            remover_cliente(viz, i, j);
+            for(int k = 0; k < num_vei; k++)
+            {
+                for(int l = 0; l <= viz.vet_qtd_cli_vei[k]; l++)
+                {
+                    inserir_cliente(viz, k, l, cli);
+                    calcular_fo(viz);
+
+                    if(viz.fo < s.fo)
+                    {
+                        memcpy(&s, &viz, sizeof(Solucao));
+                        goto INICIO;
+                    }
+
+                     remover_cliente(viz, k, l);
+                }
+            }
+
+            inserir_cliente(viz, i, j, cli);
+        }
+    }
+}
+
+void remover_cliente(Solucao& s, const int& vei, const int& pos)
+{
+    int cli = s.mat_sol[vei][pos]; // Salva antes de deslocar
+    for (int i = pos; i < s.vet_qtd_cli_vei[vei] - 1; i++)
+        s.mat_sol[vei][i] = s.mat_sol[vei][i + 1];
+    s.vet_qtd_cli_vei[vei]--;
+    s.vet_pes_vei[vei] -= vet_dem_cli[cli];
+}
+
+void inserir_cliente(Solucao& s, const int& vei, const int& pos, const int& cli)
+{
+    for (int i = s.vet_qtd_cli_vei[vei]; i > pos; i--)
+        s.mat_sol[vei][i] = s.mat_sol[vei][i - 1];
+    s.mat_sol[vei][pos] = cli;
+    s.vet_qtd_cli_vei[vei]++;
+    s.vet_pes_vei[vei] += vet_dem_cli[cli];
 }
 
 void calcular_fo(Solucao& s)
@@ -101,6 +371,7 @@ void calcular_fo(Solucao& s)
     memset(&s.vet_pes_vei, 0, sizeof(s.vet_pes_vei));
     for (int i = 0; i < num_vei; i++) // linha
     {
+        if (s.vet_qtd_cli_vei[i] == 0) continue; // evita acesso inválido
         s.fo += mat_custo[0][s.mat_sol[i][0]];
         s.vet_pes_vei[i] += vet_dem_cli[s.mat_sol[i][0]];
         for (int j = 1; j < s.vet_qtd_cli_vei[i]; j++) // coluna
@@ -112,6 +383,26 @@ void calcular_fo(Solucao& s)
         //---
         s.fo += PESO_CAP * MAX(0, s.vet_pes_vei[i] - vet_cap_vei[i]);
     }
+}
+
+void escrever_solucao(Solucao &s)
+{
+    printf("\n\n\n");
+    for (int i = 0; i < num_vei; i++)
+    {
+        for (int j = 0; j < s.vet_qtd_cli_vei[i]; j++)
+            printf("%d ", s.mat_sol[i][j]);
+        printf("\n");
+    }
+    printf("\nQTD: ");
+    for (int i = 0; i < num_vei; i++)
+        printf("%d ", s.vet_qtd_cli_vei[i]);
+
+    printf("\nPESO: ");
+    for (int i = 0; i < num_vei; i++)
+        printf("%d ", s.vet_pes_vei[i]);
+
+    printf("\n\nFO: %.2f\n", s.fo);
 }
 
 void testar_dados(char* arq)
